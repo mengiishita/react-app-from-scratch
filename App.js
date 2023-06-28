e443b2c8901ddb347797f729278f1b9d2dfc8a32
Ishita Mengi <mengiishita@gmail.com>
2023-06-28 16:02:08 +0200

    Element vs component and component composition

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "heading-1" }, "I am h1 tag"),
-    React.createElement("h2", { id: "heading-2" }, "I am h2 tag"),
-  ])
-);
+// React Element
+const heading = <h1 className="heading">Hello from JSX</h1>;
+
+// React Functional Component
+const Title = () => <h2>Title component</h2>;
+
+// Component composition
+const HeadingComponent = () => {
+  return (
+    <div className="container">
+      {/* React element inside React component */}
+      {heading}
+      <Title />
+      <h3>Functional Component</h3>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+// rendering components
+root.render(<HeadingComponent />);
